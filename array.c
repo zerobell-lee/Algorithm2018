1805d63b5bd34fcb70ab2b3867f3d35925f4665f
Zerobell Lee <bnb3456@gmail.com>
2018-03-24 18:19:21 +0900

    ver 0.9?

--- array.c
+++ array.c
@@ -1,12 +1,22 @@
+/*
+201202564 일본어학전공
+이영종
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #define MAX_LENGTH 100
+
 
 void printArray(char *name, int a[], int len) {
 	printf("%s[%d] = { ", name, len);
-	for (int i=0; i<len; i++) {
+	for (int i=0; i<len; i++) { //size가 0인 array는 내용을 출력하지 않는다.
 		printf("%d", a[i]);
 		if (i!=len-1)
-			printf(",");
+			printf(","); //마지막 요소가 아닐 경우에만 쉼표를 출력한다.
+		if (i==MAX_LENGTH)
+			break; //배열의 최대 사이즈를 넘게 출력하지 않는다.
 	}
 	printf(" }\n");
 }
@@ -29,8 +39,10 @@ void main() {
 	int list[MAX_LENGTH];
 	int size = 5;
 
+	srand(time(NULL));
+
 	for (int i=0; i<size; i++)
-		list[i] = 10 + i*10;
+		list[i] = i + rand()%20; 
 
 	printf("HW 1. Insert & Delete Element\n");
 	printArray("list", list, size);
